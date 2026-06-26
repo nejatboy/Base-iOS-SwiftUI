dd78dd6bb14616221b8f447a1d4b1d24c3dd4ba5
Nejat Boy <>
2026-06-26 17:58:22 +0300

    Project created.

---
+++ BaseSwiftUI/BaseSwiftUI/BaseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseSwiftUIApp.swift
+//  BaseSwiftUI
+//
+//  Created by Nejat Boy on 26.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct BaseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
